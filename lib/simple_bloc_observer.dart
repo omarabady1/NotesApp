895a5ae2f5c079557f3dbdb9d10c5c0baf376785
omarabady <omarabady44@gmail.com>
2025-10-02 11:25:14 +0300

    Complete Implementing BlocObserver

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -10,27 +10,27 @@ class SimpleBlocObserver implements BlocObserver
 
   @override
   void onClose(BlocBase bloc) {
-    // TODO: implement onClose
+    debugPrint('close: $bloc');
   }
 
   @override
   void onCreate(BlocBase bloc) {
-    // TODO: implement onCreate
+    debugPrint('create: $bloc');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
+    debugPrint('error: $error');
   }
 
   @override
   void onEvent(Bloc bloc, Object? event) {
-    // TODO: implement onEvent
+    debugPrint('event: $event');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    // TODO: implement onTransition
+    debugPrint('transition: $transition');
   }
 
 }
