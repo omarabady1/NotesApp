f88d8b476cf65c5fad65438833a0bace81b3a738
omarabady <omarabady44@gmail.com>
2025-10-02 22:30:00 +0300

    Delete Slider Instead of Icon

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -1,69 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
-import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
-class NoteCard extends StatelessWidget {
+import '../cubits/notes_cubit/notes_cubit.dart';
+
+class NoteCard extends StatefulWidget {
   const NoteCard({super.key, required this.cardColor, required this.note});
+
   final Color cardColor;
   final NoteModel note;
+
+  @override
+  State<NoteCard> createState() => _NoteCardState();
+}
+
+class _NoteCardState extends State<NoteCard> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, EditNoteView.id, arguments: note);
-      },
-      child: Container(
-        padding: const EdgeInsetsGeometry.symmetric(
-          vertical: 24,
-          horizontal: 24,
-        ),
-        margin: const EdgeInsetsGeometry.only(right: 22, left: 22, bottom: 20),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: cardColor,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.end,
+    return Padding(
+      padding: const EdgeInsets.only(right: 22, left: 22, bottom: 20),
+      child: Slidable(
+        key: ValueKey(widget.note.key),
+        endActionPane: ActionPane(
+          extentRatio: 0.25,
+          motion: const DrawerMotion(),
           children: [
-            ListTile(
-              contentPadding: EdgeInsetsGeometry.all(0),
-              title: Text(
-                note.title,
-                style: const TextStyle(fontSize: 24, color: Colors.black87),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  note.content,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black.withValues(alpha: 0.5),
-                  ),
-                  maxLines: 5,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              trailing: IconButton(
-                onPressed: () {
-                  note.delete();
-                  BlocProvider.of<NotesCubit>(context).fetchNotes();
-                },
-                icon: Icon(
-                  Icons.delete,
-                  size: 32,
-                  color: Colors.black.withValues(alpha: 0.8),
-                ),
-              ),
-            ),
-            Text(
-              DateFormat('EEE, d/M/y').add_jm().format(note.date),
-              style: TextStyle(color: Colors.black.withValues(alpha: 0.45)),
+            SlidableAction(
+              onPressed: (_) {
+                widget.note.delete();
+                BlocProvider.of<NotesCubit>(context).fetchNotes();
+              },
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              icon: Icons.delete,
+              label: 'Delete',
             ),
           ],
+        ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              EditNoteView.id,
+              arguments: widget.note,
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: widget.cardColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: Text(
+                    widget.note.title,
+                    style: const TextStyle(fontSize: 24, color: Colors.black87),
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: Text(
+                      widget.note.content,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black.withValues(alpha: 0.5),
+                      ),
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.keyboard_double_arrow_left_outlined,
+                    size: 32,
+                    color: Colors.black.withValues(alpha: 0.8),
+                  ),
+                ),
+                Text(
+                  DateFormat('EEE, d/M/y').add_jm().format(widget.note.date),
+                  style: TextStyle(color: Colors.black.withValues(alpha: 0.45)),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
