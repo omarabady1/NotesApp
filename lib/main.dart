6eef435d15d55ab31afe8ff3fbde2c8dbaf90742
omarabady <omarabady44@gmail.com>
2025-10-02 09:32:14 +0300

    Fix Hive Error

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:notes_app/views/homescreen.dart';
 void main() async {
   Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
-  await Hive.openBox(kNotesBox);
+  await Hive.openBox<NoteModel>(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
