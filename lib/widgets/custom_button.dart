7b7decd42d3caa5517c897aff643fac8101a6977
omarabady <omarabady44@gmail.com>
2025-10-02 17:50:43 +0300

    Reorganize Cubit Files

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:notes_app/cubits/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/widgets/custom_progress_indicator.dart';
 
 class CustomButton extends StatelessWidget {
